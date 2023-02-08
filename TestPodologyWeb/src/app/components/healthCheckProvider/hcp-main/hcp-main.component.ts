8d70359109fecd72a3269b64821884de08a468b0
tjo <tjo@LAPTOP-121>
2023-02-08 01:11:42 +0100

    add navitems

--- TestPodologyWeb/src/app/components/healthCheckProvider/hcp-main/hcp-main.component.ts
+++ TestPodologyWeb/src/app/components/healthCheckProvider/hcp-main/hcp-main.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Role } from 'src/app/models/role';
 import { User } from 'src/app/models/user';
+import { navItems } from './nav-items';
 
 @Component({
   selector: 'app-hcp-main',
@@ -12,9 +14,14 @@ export class HcpMainComponent implements OnInit {
 
   public currentUser: User | undefined;
   showFiller = false;
+  public navItems: any[] = [];
 
   ngOnInit(): void {
     this.currentUser = JSON.parse(localStorage.getItem('currentUser') as string);
+
+    this.navItems = navItems.filter((x) => x.role.indexOf(this.currentUser?.role as string) !== -1)
+
+    console.log(this.navItems)
   }
 
   logout(){
---
+++ TestPodologyWeb/src/app/models/nav-item.ts
@@ -0,0 +1,12 @@
+export interface NavItem {
+    path: string;
+    title: string;
+    iconType: string;
+    icon: string;
+    class: string;
+    groupTitle: boolean;
+    badge: string;
+    badgeClass: string;
+    role: string[];
+    submenu: NavItem[];
+}
